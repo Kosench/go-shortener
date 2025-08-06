a98910ad4e3366149b50f5b6d0cfecbca5b1feaa
Kosench <ilyakosenko09@gmail.com>
2025-08-07 04:32:23 +1000

    feat(handler): add ShortenURL and RedirectURL HTTP handlers

---
+++ internal/shortener/handler/http.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Kosench/go-shortener/internal/shortener/model"
+	"github.com/Kosench/go-shortener/internal/shortener/repository"
+	"github.com/Kosench/go-shortener/internal/shortener/service"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ShortenerHandler struct {
+	service *service.ShortenerService
+}
+
+func NewShortenerHandler(s *service.ShortenerService) *ShortenerHandler {
+	return &ShortenerHandler{service: s}
+}
+
+func (h *ShortenerHandler) ShortenURL(c *gin.Context) {
+	var req model.ShortenRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Invalid request: %v", err)})
+		return
+	}
+
+	code, err := h.service.Shorten(req.URL)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to shorten URL: %v", err)})
+		return
+	}
+
+	resp := model.ShortenResponse{ShortURL: fmt.Sprintf("http://%s/%s", c.Request.Host, code)}
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *ShortenerHandler) RedirectURL(c *gin.Context) {
+	code := c.Param("code")
+	url, err := h.service.GetOriginalURL(code)
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		}
+		return
+	}
+	c.Redirect(http.StatusMovedPermanently, url)
+}
